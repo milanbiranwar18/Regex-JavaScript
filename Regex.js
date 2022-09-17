d60cf7bc5c066bce9a9199ea24cc6669732fa83b
milanbiranwar18 <109934967+milanbiranwar18@users.noreply.github.com>
2022-09-17 14:08:12 +0530

    Update Regex.js

--- Regex.js
+++ Regex.js
@@ -4,8 +4,8 @@ let sampleRegex = RegExp('^[1-9]{1}[0-9]{2}\\s?[0-9]{3}$');
 let sampleCheck = sampleRegex.test("400 088");
 console.log(sampleCheck);
 
-let EmailRegex = RegExp('^[0-9a-zA-Z]+([+#%&_.][a-zA-Z0-9]+)*[@]{1}[a-zA-Z]{10,}[.][a-zA-Z]{2,3}([.][a-zA-Z]{2})?$');
-let EmailCheck = EmailRegex.test("abc.xyz@bridgelabz.co.in");
+let EmailRegex = RegExp('^[a-zA-Z]{3,}[@]{1}[a-zA-Z]{10,}[.][a-zA-Z]{2,3}$');
+let EmailCheck = EmailRegex.test("abc@bridgelabz.co");
 console.log(EmailCheck);
 
 
