41d663e22f821619899d0b13e6b6ed9b79cddb7e
Milan Biranwar <milanbiranwar06@gmail.com>
2022-09-17 13:43:54 +0530

    Program for validating regex pattern for Email with bridgelabz name

--- Regex.js
+++ Regex.js
@@ -4,7 +4,7 @@ let sampleRegex = RegExp('^[1-9]{1}[0-9]{2}\\s?[0-9]{3}$');
 let sampleCheck = sampleRegex.test("400 088");
 console.log(sampleCheck);
 
-let EmailRegex = RegExp('^[0-9a-zA-Z]+([+#%&_.][a-zA-Z0-9]+)*[@]{1}[a-zA-Z]{2,}[.][a-zA-Z]{2,3}([.][a-zA-Z]{2})?$');
+let EmailRegex = RegExp('^[0-9a-zA-Z]+([+#%&_.][a-zA-Z0-9]+)*[@]{1}[a-zA-Z]{10,}[.][a-zA-Z]{2,3}([.][a-zA-Z]{2})?$');
 let EmailCheck = EmailRegex.test("abc.xyz@bridgelabz.co.in");
 console.log(EmailCheck);
 
